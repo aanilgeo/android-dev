760dd017b528bb0f8133b2fcbcab60cb4358ef80
Andrew Anil George <andrewpularickal@gmail.com>
2024-10-13 21:06:27 -0400

    Removed unwanted cache

---
+++ JokeFetchingApp/app/build/generated/source/buildConfig/androidTest/debug/com/example/jokefetchingapp/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.jokefetchingapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.jokefetchingapp.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String JOKE_API_KEY = "28df24dff4msh2e0ea3ebcdddd53p1d66dfjsn97865e4fe3bc";
+}
---
+++ JokeFetchingApp/app/build/generated/source/buildConfig/debug/com/example/jokefetchingapp/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.jokefetchingapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.jokefetchingapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String JOKE_API_KEY = "28df24dff4msh2e0ea3ebcdddd53p1d66dfjsn97865e4fe3bc";
+}
